ad10a788c9aa0e96ccbdc2fab05dabe3cf22b9df
omarakamal <omaraakamal@gmail.com>
2026-02-10 19:54:58 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from 'axios'
 function App() {
   const [students, setStudents] = useState([])
 
   async function getAllStudents(){
     const allStudents = await axios.get('https://omar-ga-class.onrender.com/students/') // always async await axios calls
-    console.log(allStudents.data)
+    // setStudents(allStudents.data)
   }
   getAllStudents()
+  console.log('Component refreshes')
 
   return (
     <div>App</div>
