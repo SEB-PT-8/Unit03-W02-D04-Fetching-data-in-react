5edc278f7f885caeb072489bc3bb48622a38e5ef
omarakamal <omaraakamal@gmail.com>
2026-02-10 19:43:24 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import axios from 'axios'
+function App() {
+  const [students, setStudents] = useState([])
+
+  function getAllStudents(){
+    const allStudents = 
+  }
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
